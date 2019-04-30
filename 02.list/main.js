836f2131f7bb4dfe044c12b80be57b3cfcddfd84
khm@29cm.co.kr <khm@29cm.co.kr>
2019-04-30 16:38:58 +0900

    implement function

--- 02.list/main.js
+++ 02.list/main.js
@@ -3,20 +3,59 @@ function List() {
     this.listSize = 0;
     this.pos = 0;
     this.dataStore = []; // 리스트 요소를 저장할 빈 배열
-    this.clear = clear;
+    //this.clear = clear;
     this.find = find;
     this.toString = toString;
-    this.insert = insert;
+    //this.insert = insert;
     this.append = append;
     this.remove = remove;
-    this.front = front;
-    this.end = end;
-    this.prev = prev;
-    this.next = next;
+   // this.front = front;
+   // this.end = end;
+   // this.prev = prev;
+   // this.next = next;
     this.length = length; // 길이 반환
-    this.currPos = currPos;
-    this.moveTo = moveTo;
-    this.getElement = getElement;
-    this.contains = contains;
+   // this.currPos = currPos;
+   // this.moveTo = moveTo;
+  //  this.getElement = getElement;
+  //  this.contains = contains;
 
 }
+
+function find(ele) {
+    for(var i=0; i<this.dataStore.length; ++i){
+        if(this.dataStore[i] == ele) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+function append(ele) {
+    this.dataStore[this.listSize++] = ele;
+}
+
+function remove(ele) {
+    var idx = this.find(ele);
+    if(idx > -1){
+        this.dataStore.splice(idx,1);
+        --this.listSize;
+        return true;
+    }
+    return false;
+}
+
+function length() {
+    return this.listSize;
+}
+
+function toString() {
+    return this.dataStore;
+}
+
+var names = new List();
+names.append('A');
+names.append('b');
+names.append('C');
+console.log(names.toString());
+names.remove('b');
+console.log(names.toString());
