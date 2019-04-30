c976b141daade285c8206163ca325da081b5dbf2
khm@29cm.co.kr <khm@29cm.co.kr>
2019-04-30 16:58:17 +0900

    implement function

--- 02.list/main.js
+++ 02.list/main.js
@@ -3,21 +3,21 @@ function List() {
     this.listSize = 0;
     this.pos = 0;
     this.dataStore = []; // 리스트 요소를 저장할 빈 배열
-    //this.clear = clear;
+    this.clear = clear;
     this.find = find;
     this.toString = toString;
-    //this.insert = insert;
+    this.insert = insert;
     this.append = append;
     this.remove = remove;
-   // this.front = front;
-   // this.end = end;
-   // this.prev = prev;
-   // this.next = next;
+    this.front = front;
+    this.end = end;
+    this.prev = prev;
+    this.next = next;
     this.length = length; // 길이 반환
-   // this.currPos = currPos;
-   // this.moveTo = moveTo;
-  //  this.getElement = getElement;
-  //  this.contains = contains;
+    this.currPos = currPos;
+    this.moveTo = moveTo;
+    this.getElement = getElement;
+    this.contains = contains;
 
 }
 
@@ -52,10 +52,81 @@ function toString() {
     return this.dataStore;
 }
 
+function insert(ele, afterEle) {
+    var tgIdx = this.find(afterEle);
+    if(tgIdx>-1){
+        this.dataStore.splice(tgIdx,1, ele);
+        ++this.listSize;
+        return true;
+    }
+    return false;
+}
+
+function clear() {
+    delete this.dataStore;
+    this.dataStore.length = 0;
+    this.listSize = 0;
+}
+
+function contains(ele) {
+    for(var i =0; i<this.dataStore.length; ++i){
+        if(this.dataStore[i] == ele) {
+            return true;
+        }
+    }
+    return false;
+}
+
+// 현재 위치를 리스트의 첫번째 요소로 결정
+function front() {
+    this.pos = 0;
+}
+// 현재 위치를 리스트의 마지막 요소로 결정 
+function end() {
+    this.pos = this.listSize -1;
+}
+// 현재 위치를 한 요소 뒤로 이동
+function prev() {
+    if(this.pos > 0) {
+        --this.pos;
+    }
+}
+// 현재 위치를 한 요소 앞으로 이동
+function next() {
+    if(this.pos<this.listSize-1) {
+        ++this.pos;
+    }
+}
+// 리스트의 현재 위치를 반환
+function currPos() {
+    return this.pos;
+}
+// 현재 위치를 지정된 위치로 이동
+function moveTo(pos) {
+    this.pos = pos;
+}
+
+//현재 위치의 요소를 반환
+function getElement(){
+    return this.dataStore[this.pos];
+}
+
+
 var names = new List();
 names.append('A');
 names.append('b');
 names.append('C');
-console.log(names.toString());
-names.remove('b');
-console.log(names.toString());
+names.append('d');
+names.append('e');
+names.append('f');
+
+names.front();
+console.log(names.getElement())
+
+names.next();
+console.log(names.getElement())
+
+names.next();
+names.next();
+names.prev();
+console.log(names.getElement())
