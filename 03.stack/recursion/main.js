ae2a9119b0b1feca826177bf3b286acae33697ac
khm <khm@29cm.co.kr>
2019-05-09 17:08:43 +0900

    recursion 추가

---
+++ 03.stack/recursion/main.js
@@ -0,0 +1,52 @@
+function Stack() {
+    this.dataStore = []; // 스택 요소를 저장하는 배열
+    this.top = 0;       // 스택탑 정보를 유지하며 생성자에서 0으로 초기화
+    this.push = push;   // 요소 추가, 추가시 top을 증가 시켜 새로운 top으로 가리키도록 한다.
+    this.pop = pop;    // 요소 삭제, top을 감소시켜 탑 위치를 변경
+    this.peek = peek;  // 현재 위치요소에 접근해서 반환
+    this.clear = clear;
+    this.length = length;
+}
+
+function push(ele) {
+    this.dataStore[this.top++] = ele;
+}
+
+function pop() {
+   return this.dataStore[--this.top];
+}
+
+function peek() {
+    return this.dataStore[this.top-1];
+}
+function length() {
+    return this.top;   // top을 반환하여 stack에 데이터가 얼마나 저장되어 있는지 알 수 있다.
+}
+function clear() {
+    this.top = 0;   // 스택의 전체 요소 삭제
+}
+
+function factorial(n) {
+    
+    if (n > 0) {
+        return n * factorial(n-1);
+    } else {
+        return 1;
+    }
+}
+
+function fact(n) {
+    var s = new Stack();
+    while(n>0){
+        s.push(n--);  // n push후 가감
+    }
+    
+    var res = 1;
+    while(s.length() > 0) {
+        res *= s.pop();
+    }
+    return res;
+}
+
+console.log(fact(5));
+console.log(factorial(5));
